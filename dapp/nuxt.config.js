0c104c9d0540a433df413f824216ee676c586889
x <x@to.wtf>
2021-06-07 22:04:07 +0300

    :zap:

--- dapp/nuxt.config.js
+++ dapp/nuxt.config.js
@@ -15,8 +15,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'gutter cat gang',
-    title: 'gutter cat gang',
+    titleTemplate: 'Gutter Cat Gang',
+    title: 'Gutter Cat Gang',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
