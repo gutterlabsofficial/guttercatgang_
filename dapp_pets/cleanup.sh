79c46a4f06a6e8bc4513c80574489516eb77d24b
x <x@to.wtf>
2021-06-29 15:26:52 +0300

    vesting

--- dapp_pets/cleanup.sh
+++
@@ -1,4 +0,0 @@
-rm -rf node_modules
-rm package-lock.json
-npm install
-npm run dev
--- dapp_pets/constants.js
+++
@@ -1,5 +0,0 @@
-export const CONTRACT_ADDR = '0xEdB61f74B0d09B2558F1eeb79B247c1F363Ae452'
-export const NETWORK_ID = 1
-export const RPC_PROVIDER =
-  'https://eth-mainnet.alchemyapi.io/v2/MVUUaEb2SO3ZuMN9P0M1wBGWgPTEsZ8l'
-export const DEADLINE = '06/08/2021 20:20:00 UTC'
